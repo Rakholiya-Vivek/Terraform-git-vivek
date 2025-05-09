9b03d5d133e29df0e9ae21538ad6f671ce88f609
Rakholiya-Vivek <rakholiyavivek2004@gmail.com>
2025-05-09 18:36:55 +0530

    day-8

--- day-8-modules/main.tf
+++ day-8-modules/main.tf
@@ -1,5 +1,17 @@
-module "test" {
-    source = "../day-8-module-source"
-    ami = "ami-0f88e80871fd81e91"
-    type = "t2.micro"
+# module "name" {
+    # source = ""
+    # ami = "ami-0f88e80871fd81e91"
+    # type = "t2.micro"
+    # vpc_id = "vpc-085711b82cae8cf09"
+# }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
