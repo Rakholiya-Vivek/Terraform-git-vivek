6a3bd212cf36def0ef41115368fc734b913bbae2
Rakholiya-Vivek <rakholiyavivek2004@gmail.com>
2025-05-09 16:57:06 +0530

    Update main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "name" {
-  ami = var.ami
-  instance_type = var.type
+resource "aws_subnet" "name" {
+    cidr_block = "172.31.96.0/20"
+    vpc_id = var.vpc_id
+
+    tags = {
+      Name = "def-sub"
+    }
 }
